71a305f7cc03844a081c7f9d16bc1cb0b7ce094b
sbo46eka <153824642+sbo46eka@users.noreply.github.com>
2025-02-28 12:50:06 +0200

    Create ussd.php

---
+++ ussd.php
@@ -0,0 +1,6 @@
+To do list 
+list all new task on the table
+list them with due date
+we have button add,edit and delete 
+we also have button delete all that ask you sure you want to delete 
+search bar for quick browsing
